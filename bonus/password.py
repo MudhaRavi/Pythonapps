4cdd061ae416b134ed84cbf6863889ed074c5759
Mudharavi <rkcanada9@gmail.com>
2025-10-10 00:34:22 -0400

    Day_10

--- bonus/password.py
+++ bonus/password.py
@@ -27,3 +27,20 @@ else:
     print("Weak password")
 
 
+
+# Percentage calculator
+try:
+    total_value = float(input("Enter total value: "))
+    value = float(input("Enter value: "))
+    percentage = value/total_value * 100
+    print(f"That is {percentage}%")
+
+except ValueError and ZeroDivisionError:
+    if  ValueError:
+        print("Total value cannot be zero. Run the program again.")
+    elif ZeroDivisionError:
+        print("You need to enter a number.Run the program again.")
+
+
+
+
