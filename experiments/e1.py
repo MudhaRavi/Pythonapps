956dba0ac32561ca379df0e93e1a14bec8e31c92
Mudharavi <rkcanada9@gmail.com>
2025-10-14 15:52:38 -0400

    Day_12

--- experiments/e1.py
+++ experiments/e1.py
@@ -1,13 +1,17 @@
-user_prompt = " Enter your prompt: "
-todo_list = []
+# user_prompt = " Enter your prompt: "
+# todo_list = []
+#
+# while True:
+#
+#     todo = input(user_prompt)
+#     todo_list.append(todo)
+#     if todo == "exit":
+#         break
+# print(f"Todo: {todo_list}")
 
-while True:
 
-    todo = input(user_prompt)
-    todo_list.append(todo)
-    if todo == "exit":
-        break
-print(f"Todo: {todo_list}")
+def liters_to_m3(liters):
+    cubic = liters/1000
+    return cubic
 
-
-
+print(liters_to_m3(5))  # Example usage
