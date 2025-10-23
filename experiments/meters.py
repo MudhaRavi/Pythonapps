af37cd51f8cbf35eadc821c66849a0621e9a4e4d
Mudharavi <rkcanada9@gmail.com>
2025-10-23 15:22:33 -0400

    Day_19

--- experiments/meters.py
+++ experiments/meters.py
@@ -1,3 +1,11 @@
-def convert(feet, inches):
-    meter = feet * 0.03048 + inches * 0.0254
-    return meter
+# def convert(feet, inches):
+#     meter = feet * 0.03048 + inches * 0.0254
+#     return meter
+
+def fluid(ounces):
+    convertor = ounces * 29.57353
+    return convertor
+
+
+milliliters = fluid(1)
+print(milliliters)
